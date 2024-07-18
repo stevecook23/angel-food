865a8c9c3b6f6e69016ce9679b78ba1ea0a1a159
Steven Cook <stevecook23@gmail.com>
2024-07-18 15:30:24 +0000

    Added function to register

--- app.py
+++ app.py
@@ -29,6 +29,27 @@ def show_places():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        
+        # Checking if the username already exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        # Message if the username already exists
+        if existing_user:
+            flash("Username already exists!")
+            return redirect(url_for("register"))
+
+        # Adding the new user
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # Add the new user to the current session's cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration successful! Hold tight...")
     return render_template("register.html")
 
 

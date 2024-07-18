dc22a8f9f6391ae9ef3f7cbdd4ec4377924088d1
Steven Cook <stevecook23@gmail.com>
2024-07-18 20:27:13 +0000

    Troubleshooting 'email' input not working

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,7 @@ $(document).ready(function () {
   $(".sidenav").sidenav({ edge: "right" });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  M.AutoInit();
+  M.updateTextFields();
+});
